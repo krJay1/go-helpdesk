3be1379276c4bb2d2d35d09565e57da04708c9bf
krJay1 <krjaydeep1@gmail.com>
2026-05-31 17:08:01 +0530

    Graceful shutdown implemented

--- cmd/helpdesk/main.go
+++ cmd/helpdesk/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -19,6 +24,32 @@ func main() {
 	root := mux.NewRouter()
 	root.HandleFunc("/", HomeHandler)
 
+	addr := ":8088"
+
 	log.Println("Server is starting on port :8088")
-	http.ListenAndServe(":8088", root)
+	server := http.Server{
+		Addr:    addr,
+		Handler: root,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+	log.Println("Shutting down server...")
+
+	// Give active request 5 seconds to execute
+	ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancle()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Shutdown error: %v", err)
+	}
+
+	log.Println("Server shutdown gracefully.")
 }
