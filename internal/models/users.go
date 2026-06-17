0a472aa03214acc0c62fc137537e1d1802fd11da
krJay1 <krjaydeep1@gmail.com>
2026-06-17 12:01:27 +0530

    Users table setup

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,7 +1,14 @@
 package models
 
 type User struct {
-	ID    int64  `json:"id"`
-	Email string `json:"email"`
-	Name  string `json:"name"`
+	ID           int64  `json:"id"`
+	Email        string `json:"email"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	MobileNumber string `json:"mobile_number"`
+	PasswordHash string `json:"-"`
+	LastLogin    string `json:"last_login"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
+	IsActive     bool   `json:"is_active"`
 }
